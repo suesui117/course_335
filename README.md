eb7967745cd32ff99b651d5006ef318324794767
Sue Sui <sue@SuetekiMacBook-Pro.local>
2024-06-18 21:21:10 -0400

    User defined copy-constructor/copy-assignment are deep copy,

--- README.md
+++
@@ -1,4 +0,0 @@
-# CSCI335 Software Design and Analysis 3 Summer 2024
-## Projects
-
-[Forked from: Jaime Canizales](https://github.com/canizalesjaime/course_335)
--- class_code/week2/06-05_big5_constructors/tempCodeRunnerFile.cpp
+++
@@ -1,6 +0,0 @@
-        Entity(const Entity& other)//copy constuctor
-        {
-            cout<<"copy constructor called"<<endl;
-            id_ = other.id_;
-            data_ = other.data_;
-        }

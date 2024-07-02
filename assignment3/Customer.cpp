a9a733ce1168d0c7c5f3891aad43086845379008
Sue Sui <sue@SuetekiMacBook-Pro.local>
2024-07-01 21:54:54 -0400

    Customer.cpp

---
+++ assignment3/Customer.cpp
@@ -0,0 +1,58 @@
+#ifndef CUSTOMER_H
+#define CUSTOMER_H
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cstdlib> // defines a core set of functions used for data type conversion, 
+// pseudo-random number generation, memory allocation, searching, sorting, mathematics, 
+// and dealing with wide or multibyte characters.
+
+class Customer
+{
+    public:
+        Customer()
+        {
+            name_ = "";
+            service_time_ = 0;
+            SetPriorityLevel();
+        }// default constructor
+        
+        Customer(const std::string name, const int service_time, const int priority_level )
+        {
+            name_ = name;
+            service_time_ = service_time;
+            SetPriorityLevel();
+        }
+
+        void SetPriorityLevel()
+        {
+            priority_level_ = rand() % 101; // Generates a pseudo-random integer between 0 and RAND_MAX.
+            // mod 101 because we need 0 to 100, if 101/101, that loops back to 0. 
+            std::cout << priority_level_ << "\n";
+        };
+
+        void SetServiceTime()
+        {
+            service_time_ = rand() % 61; // 0 to 60
+        };
+
+
+    private:
+        std::string name_;
+        int service_time_;
+        int priority_level_;
+
+};
+
+
+#endif // end of customer class
+
+
+
+int main()
+{
+
+    Customer sue("sue", 5, 5);
+    sue.SetPriorityLevel();
+}
---
+++ assignment3/maxHeap.h
@@ -0,0 +1,20 @@
+#ifndef MAXHEAP_H
+#define MAXHEAP_H
+
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+/* maxHeap is a template class 
+priority queue is a queue that supports a deleteMin and an insert operations, no other operations are required
+
+*/
+
+template<typename T>
+class maxHeap
+{
+
+};
+
+
+#endif
